d4efc0d30f1f683820bd46fd774aa4a7fc0200d3
Junichi Kato <j5ik2o@gmail.com>
2008-06-12 03:04:02 +0000

    git-svn-id: https://www.seasar.org/svn/sandbox/s2config/trunk@20 6391a11f-5a70-4f64-bf81-fabb485c03d3

--- s2config-core/src/main/java/org/seasar/config/core/container/ConfigContainer.java
+++ s2config-core/src/main/java/org/seasar/config/core/container/ConfigContainer.java
@@ -1,17 +1,28 @@
-package org.seasar.config.core.manager;
+package org.seasar.config.core.container;
 
 import org.seasar.config.core.config.ConfigReader;
 import org.seasar.config.core.config.ConfigWriter;
+import org.seasar.framework.container.S2Container;
 import org.seasar.framework.util.Disposable;
 
-public class ConfigManager implements Disposable {
+public class ConfigContainer implements Disposable {
 
 	private String configName;
 	private ConfigWriter configWriter;
 	private ConfigReader configReader;
+	private ConfigContainer childContainer;
+	private S2Container s2Container;
 
 	public void setConfigName(String configName) {
 		this.configName = configName;
+	}
+
+	public String getConfigName() {
+		return configName;
+	}
+
+	public void setS2Container(S2Container container) {
+		s2Container = container;
 	}
 
 	public void setConfigWriter(ConfigWriter configWriter) {
@@ -26,12 +37,20 @@ public class ConfigManager implements Disposable {
 		configReader.open(configName);
 		String env = configReader.readConfigValue("env", null);
 		if (env != null) {
-
+			childContainer = (ConfigContainer) s2Container
+					.getComponent(ConfigContainer.class);
+			String[] configNameSplit = configName.split("\\.");
+			childContainer.setConfigName(String.format("%s_%s.properties",
+					configNameSplit[0], env));
+			childContainer.initialize();
 		}
 	}
 
 	public void dispose() {
 		configReader.close();
+		if (this.childContainer != null) {
+			this.childContainer.dispose();
+		}
 	}
 
 }
