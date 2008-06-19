ea9efddedd9c010260db91af06975234377e16ea
Junichi Kato <j5ik2o@gmail.com>
2008-06-19 03:04:25 +0000

    Customize.dicon対応
    git-svn-id: https://www.seasar.org/svn/sandbox/s2config/trunk@43 6391a11f-5a70-4f64-bf81-fabb485c03d3

--- s2config-core/src/main/java/org/seasar/config/core/util/DiconConditionUtil.java
+++ s2config-core/src/main/java/org/seasar/config/core/util/DiconConditionUtil.java
@@ -6,8 +6,13 @@ public class DiconConditionUtil {
 
 	private static final String CONVENTION_DICON = "convention.dicon";
 
+	private static final String CUSTOMIZE_DICON = "configCustomize.dicon";
+
 	public static boolean hasConvention() {
 		return ResourceUtil.getResourceNoException(CONVENTION_DICON) != null;
 	}
 
+	public static boolean hasCustomize() {
+		return ResourceUtil.getResourceNoException(CUSTOMIZE_DICON) != null;
+	}
 }
