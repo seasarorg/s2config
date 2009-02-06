be11ac54a953e7f04d497bcdc8819365c45331ef
Junichi Kato <j5ik2o@gmail.com>
2009-02-06 01:39:04 +0000

    コメントを追加
    git-svn-id: https://www.seasar.org/svn/sandbox/s2config/trunk@76 6391a11f-5a70-4f64-bf81-fabb485c03d3

--- s2config-core/src/main/java/org/seasar/config/core/autodetector/ConfigClassAutoDetector.java
+++ s2config-core/src/main/java/org/seasar/config/core/autodetector/ConfigClassAutoDetector.java
@@ -1,8 +1,5 @@
 package org.seasar.config.core.autodetector;
 
-import java.lang.annotation.Annotation;
-import java.util.List;
-
 import org.seasar.config.core.config.ConfigValidator;
 import org.seasar.framework.autodetector.impl.AbstractClassAutoDetector;
 import org.seasar.framework.container.annotation.tiger.Binding;
@@ -13,16 +10,20 @@ import org.seasar.framework.util.ClassUtil;
 import org.seasar.framework.util.ResourcesUtil;
 import org.seasar.framework.util.ClassTraversal.ClassHandler;
 import org.seasar.framework.util.ResourcesUtil.Resources;
-import org.seasar.framework.util.tiger.CollectionsUtil;
 import org.seasar.framework.util.tiger.ReflectionUtil;
 
+/**
+ * Configクラスを自動的に認識するクラスです。
+ * 
+ * @author kato
+ */
 public class ConfigClassAutoDetector extends AbstractClassAutoDetector {
 	private static final String PACKAGE_NAME_CONFIG = "config";
 
 	private static final String PACKAGE_NAME_DTO = "dto";
 
-	protected final List<Class<? extends Annotation>> annotations =
-		CollectionsUtil.newArrayList();
+	// protected final List<Class<? extends Annotation>> annotations =
+	// CollectionsUtil.newArrayList();
 
 	protected NamingConvention namingConvention;
 
@@ -30,41 +31,70 @@ public class ConfigClassAutoDetector extends AbstractClassAutoDetector {
 
 	private ConfigValidator configValidator;
 
+	/**
+	 * コンストラクタです。
+	 */
 	public ConfigClassAutoDetector() {
 		// this.annotations.add(Task.class);
 	}
 
+	/**
+	 * {@link NamingConvention}を設定します。
+	 * 
+	 * @param namingConvention
+	 *            {@link NamingConvention}
+	 */
 	@Binding(bindingType = BindingType.MAY)
 	public void setNamingConvention(final NamingConvention namingConvention) {
 		this.namingConvention = namingConvention;
 	}
 
+	/**
+	 * {@link ClassLoader}を設定します。
+	 * 
+	 * @param classLoader
+	 *            {@link ClassLoader}
+	 */
 	@Binding(bindingType = BindingType.MAY)
 	public void setClassLoader(final ClassLoader classLoader) {
 		this.classLoader = classLoader;
 	}
 
-	private void addTargetPackageSubName(String packageSubName) {
+	/**
+	 * 認識するサブパッケージ名を追加します。
+	 * 
+	 * @param packageSubName
+	 *            サブパッケージ名
+	 */
+	private void addTargetSubPackageName(String subPackageName) {
 		for (final String rootPackageName : namingConvention
 			.getRootPackageNames()) {
 			final String packageName =
-				ClassUtil.concatName(rootPackageName, packageSubName);
+				ClassUtil.concatName(rootPackageName, subPackageName);
 			addTargetPackageName(packageName);
 		}
 	}
 
+	/**
+	 * コンポーネントの初期化処理です。
+	 */
 	@InitMethod
 	public void init() {
 		if (namingConvention != null) {
-			addTargetPackageSubName(PACKAGE_NAME_CONFIG);
-			addTargetPackageSubName(PACKAGE_NAME_DTO);
+			addTargetSubPackageName(PACKAGE_NAME_CONFIG);
+			addTargetSubPackageName(PACKAGE_NAME_DTO);
 		}
 	}
 
-	public void addAnnotation(final Class<? extends Annotation> annotation) {
-		// this.annotations.add(annotation);
-	}
+	// public void addAnnotation(final Class<? extends Annotation> annotation) {
+	// // this.annotations.add(annotation);
+	// }
 
+	/*
+	 * (非 Javadoc)
+	 * 
+	 * @see org.seasar.framework.autodetector.ClassAutoDetector#detect(org.seasar.framework.util.ClassTraversal.ClassHandler)
+	 */
 	@SuppressWarnings("unchecked")
 	public void detect(final ClassHandler handler) {
 		for (int i = 0; i < getTargetPackageNameSize(); i++) {
@@ -73,7 +103,6 @@ public class ConfigClassAutoDetector extends AbstractClassAutoDetector {
 				.getResourcesTypes(packageName)) {
 				try {
 					resources.forEach(new ClassHandler() {
-
 						public void processClass(final String packageName,
 							final String shortClassName) {
 							if (packageName.startsWith(packageName)
@@ -91,6 +120,15 @@ public class ConfigClassAutoDetector extends AbstractClassAutoDetector {
 		}
 	}
 
+	/**
+	 * Configクラスかどうかを返します。
+	 * 
+	 * @param packageName
+	 *            パッケージ名
+	 * @param shortClassName
+	 *            ショートクラス名
+	 * @return Configクラスであるならtrue, そうでないならfalse。
+	 */
 	protected boolean isConfig(final String packageName,
 		final String shortClassName) {
 		final String name = ClassUtil.concatName(packageName, shortClassName);
@@ -101,6 +139,13 @@ public class ConfigClassAutoDetector extends AbstractClassAutoDetector {
 		return false;
 	}
 
+	/**
+	 * クラスを返します。
+	 * 
+	 * @param className
+	 *            クラス名(FQCN)
+	 * @return クラス
+	 */
 	protected Class<?> getClass(final String className) {
 		if (classLoader != null) {
 			return ReflectionUtil.forName(className, classLoader);
@@ -108,6 +153,12 @@ public class ConfigClassAutoDetector extends AbstractClassAutoDetector {
 		return ReflectionUtil.forNameNoException(className);
 	}
 
+	/**
+	 * {@link ConfigValidator}を設定します。
+	 * 
+	 * @param configValidator
+	 *            {@link ConfigValidator}
+	 */
 	public void setConfigValidator(ConfigValidator configValidator) {
 		this.configValidator = configValidator;
 	}
