0d6a111e07cc8ebd183d2f9ba4abeb95232ab703
Junichi Kato <j5ik2o@gmail.com>
2009-02-06 02:05:48 +0000

    コメントを追加
    git-svn-id: https://www.seasar.org/svn/sandbox/s2config/trunk@78 6391a11f-5a70-4f64-bf81-fabb485c03d3

--- s2config-extension/src/main/java/org/seasar/config/extension/servlet/filter/ConfigFilter.java
+++ s2config-extension/src/main/java/org/seasar/config/extension/servlet/filter/ConfigFilter.java
@@ -12,18 +12,35 @@ import javax.servlet.ServletResponse;
 import org.seasar.config.extension.servlet.filter.command.FilterCommand;
 import org.seasar.config.extension.servlet.filter.command.FilterCommandFactory;
 
+/**
+ * S2Config用フィルターです。
+ * 
+ * @author j5ik2o
+ */
 public class ConfigFilter implements Filter {
 
+	/*
+	 * (非 Javadoc)
+	 * @see javax.servlet.Filter#destroy()
+	 */
 	public void destroy() {
 		FilterCommandFactory.clearTargetURIs();
 	}
 
+	/*
+	 * (非 Javadoc)
+	 * @see javax.servlet.Filter#doFilter(javax.servlet.ServletRequest, javax.servlet.ServletResponse, javax.servlet.FilterChain)
+	 */
 	public void doFilter(ServletRequest request, ServletResponse response,
 		FilterChain filterChain) throws IOException, ServletException {
 		FilterCommand cmd = FilterCommandFactory.create(request, response);
 		cmd.execute(request, response, filterChain);
 	}
 
+	/*
+	 * (非 Javadoc)
+	 * @see javax.servlet.Filter#init(javax.servlet.FilterConfig)
+	 */
 	public void init(FilterConfig filterConfig) throws ServletException {
 		String targetURIs = filterConfig.getInitParameter("targetURIs");
 		String[] targetURIArray = targetURIs.split(",");
