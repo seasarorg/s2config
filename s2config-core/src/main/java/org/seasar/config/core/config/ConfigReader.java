415bf34f46cae00ee44f9573c52a6d67e90c10fd
Junichi Kato <j5ik2o@gmail.com>
2009-02-10 10:08:14 +0000

    Javadocを追加しました。
    git-svn-id: https://www.seasar.org/svn/sandbox/s2config/trunk@82 6391a11f-5a70-4f64-bf81-fabb485c03d3

--- s2config-core/src/main/java/org/seasar/config/core/config/ConfigReader.java
+++ s2config-core/src/main/java/org/seasar/config/core/config/ConfigReader.java
@@ -2,17 +2,66 @@ package org.seasar.config.core.config;
 
 import java.util.Map;
 
+/**
+ * 設定情報を読み込むためのインターフェイスです。
+ * 
+ * @author j5ik2o
+ */
 public interface ConfigReader {
+	/**
+	 * 設定情報を{@link Map}に変換して返します。
+	 * 
+	 * @return {@link Map}
+	 */
 	public Map<String, Object> toMap();
 
+	/**
+	 * 設定情報を{@link Map}から読み込みます。
+	 * 
+	 * @param configResource
+	 *            設定情報
+	 */
 	public void load(Map<String, Object> configResource);
 
+	/**
+	 * 設定情報を開きます。
+	 * 
+	 * @param configName
+	 *            設定名
+	 */
 	public void open(String configName);
 
+	/**
+	 * 設定値を読み込みます。
+	 * 
+	 * @param <T>
+	 *            設定値の型
+	 * @param resultClass
+	 *            設定値のクラス
+	 * @param key
+	 *            キー
+	 * @param defaultValue
+	 *            デフォルト値
+	 * @return 設定値
+	 */
 	public <T> T readConfigValue(Class<T> resultClass, String key,
-		T defaultValue);
+			T defaultValue);
 
+	/**
+	 * 設定値を読み込みます。
+	 * 
+	 * @param <T>
+	 *            設定値の型
+	 * @param resultClass
+	 *            設定値のクラス
+	 * @param key
+	 *            キー
+	 * @return 設定値
+	 */
 	public <T> T readConfigValue(Class<T> resultClass, String key);
 
+	/**
+	 * 設定情報を閉じます。
+	 */
 	public void close();
 }
