a8f99d3be185b57df7b337840e115a70dff72c4d
Junichi Kato <j5ik2o@gmail.com>
2009-06-23 17:27:16 +0000

    finalを廃止
    git-svn-id: https://www.seasar.org/svn/sandbox/s2config/trunk@145 6391a11f-5a70-4f64-bf81-fabb485c03d3

--- s2config-example/src/main/java/org/seasar/config/example/config/Log4JConfig.java
+++ s2config-example/src/main/java/org/seasar/config/example/config/Log4JConfig.java
@@ -19,7 +19,7 @@ import org.seasar.config.core.config.annotation.Config;
 import org.seasar.config.core.config.annotation.ConfigKey;
 
 @Config(name = "log4j")
-public final class Log4JConfig {
+public class Log4JConfig {
 
 	@ConfigKey(name = "log4j.category.org.seasar")
 	public String category;
