3b3222623b7d804f2c62e1a7cb2de2ac3f1283e1
happy_ryo <iwama.ryo.0731@gmail.com>
2009-06-28 11:30:11 +0000

    readOnly関連のテストを別途作成したのでこちらからは削除。
    git-svn-id: https://www.seasar.org/svn/sandbox/s2config/trunk@161 6391a11f-5a70-4f64-bf81-fabb485c03d3

--- s2config-core/src/test/java/org/seasar/config/core/container/ConfigInjectorTest.java
+++ s2config-core/src/test/java/org/seasar/config/core/container/ConfigInjectorTest.java
@@ -10,30 +10,27 @@ import org.seasar.framework.unit.Seasar2;
 
 /**
  * ConfigInjectorクラスのテスト
- *
+ * 
  * @author j5ik2o
  * @author happy_ryo
  */
 @RunWith(Seasar2.class)
 public class ConfigInjectorTest {
+	private ConfigContainer configContainer;
 
-    private ConfigContainer configContainer;
-    private ApplicationConfig applicationConfig;
-    private ApplicationConfigDto applicationConfigDto;
+	private ApplicationConfig applicationConfig;
 
-    @Test
-    public void testInject() {
-        configContainer.setConfigName("test");
-        configContainer.loadToBeans();
-        assertEquals("abcdef", applicationConfig.hoge);
-        assertEquals(false, applicationConfig.debug);
-        assertEquals("readOnly", applicationConfig.readOnlyTrue);
-        assertEquals("readOnlyFalse", applicationConfig.readOnlyFalse);
-        assertEquals(Integer.valueOf(100), applicationConfig.xxx);
-        assertEquals("abcdef", applicationConfigDto.hoge);
-        assertEquals(false, applicationConfigDto.debug);
-        assertEquals(Integer.valueOf(100), applicationConfigDto.xxx);
-        assertEquals("readOnly", applicationConfigDto.readOnlyTrue);
-        assertEquals("readOnlyFalse", applicationConfigDto.readOnlyFalse);
-    }
+	private ApplicationConfigDto applicationConfigDto;
+
+	@Test
+	public void testInject() {
+		configContainer.setConfigName("test");
+		configContainer.loadToBeans();
+		assertEquals("abcdef", applicationConfig.hoge);
+		assertEquals(false, applicationConfig.debug);
+		assertEquals(Integer.valueOf(100), applicationConfig.xxx);
+		assertEquals("abcdef", applicationConfigDto.hoge);
+		assertEquals(false, applicationConfigDto.debug);
+		assertEquals(Integer.valueOf(100), applicationConfigDto.xxx);
+	}
 }
