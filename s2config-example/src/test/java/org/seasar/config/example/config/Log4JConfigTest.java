85a158177d2a81f9b953bfaca62ab1f6c59caa96
Junichi Kato <j5ik2o@gmail.com>
2009-10-07 16:41:30 +0000

    テストを追加しました
    git-svn-id: https://www.seasar.org/svn/sandbox/s2config/trunk@181 6391a11f-5a70-4f64-bf81-fabb485c03d3

--- s2config-example/src/test/java/org/seasar/config/example/config/Log4JConfigTest.java
+++ s2config-example/src/test/java/org/seasar/config/example/config/Log4JConfigTest.java
@@ -1,9 +1,11 @@
 package org.seasar.config.example.config;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.seasar.config.core.container.ConfigContainer;
+import org.seasar.framework.container.SingletonS2Container;
 import org.seasar.framework.unit.Seasar2;
 
 @RunWith(Seasar2.class)
@@ -14,6 +16,15 @@ public class Log4JConfigTest {
 	@Test
 	public void test() {
 		assertNotNull(log4JConfig);
+		System.out.println(log4JConfig.category);
+	}
+
+	@Test
+	public void test2() {
+		ConfigContainer config = SingletonS2Container
+				.getComponent(ConfigContainer.class);
+		System.out.println(config.getConfigValue(String.class,
+				"log4j.category.org.seasar"));
 	}
 
 }
